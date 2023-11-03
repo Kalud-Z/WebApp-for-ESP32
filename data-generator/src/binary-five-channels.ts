b5ad0820cc263d311466dee00ffc426c95b9e83c
Khaled Fahem <k.fahem@enbw.com>
2023-11-03 17:31:40 +0100

    server: setting up serveer on seperate machine

--- data-generator/src/binary-five-channels.ts
+++ data-generator/src/binary-five-channels.ts
@@ -119,7 +119,10 @@ function bytesToKilobytes(bytes: number) {
 
 
 //#######################################################################################################################
-server.listen(process.env.PORT || 8999, () => {
+
+// Start our server on all IPv4 addresses available to the operating system.
+server.listen(Number(process.env.PORT) || 8999, '0.0.0.0', () => {
     const port = (server.address() as AddressInfo).port;
-    console.log(`Binary - Five channels | Server started on port ${port} :)`);
+    console.log(`Server started on port ${port}`);
 });
+
