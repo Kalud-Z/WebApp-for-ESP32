137223c7862f4289bf1d0524fcff5c4c80e70dd0
Khaled Fahem <khaled.fhm@live.fr>
2023-11-14 16:22:35 +0100

    testing 20dp now

--- webApp/websocketModule.js
+++ webApp/websocketModule.js
@@ -7,7 +7,7 @@ export let numberOfChannels;
 
 
 function startTheApp() {
-    const numberOfDataPointsPerBatch = 10; //TODO : calculate this dynamically.
+    const numberOfDataPointsPerBatch = 20; //TODO : calculate this dynamically.
 
     let totalReceivedBytes = 0;
     let totalDataPointsReceived = 0;
