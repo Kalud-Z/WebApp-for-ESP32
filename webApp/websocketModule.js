d80ea1e9082a8f83965cbc0b3f2d86d800eaff97
Khaled Fahem <khaled.fhm@live.fr>
2023-11-23 20:01:28 +0100

    now gonna re-run tests for server running locally

--- webApp/websocketModule.js
+++ webApp/websocketModule.js
@@ -7,7 +7,7 @@ export let numberOfChannels;
 
 
 function startTheApp() {
-    const numberOfDataPointsPerBatch = 250; //TODO : calculate this dynamically.
+    const numberOfDataPointsPerBatch = 20; //TODO : calculate this dynamically.
 
     let totalReceivedBytes = 0;
     let totalDataPointsReceived = 0;
